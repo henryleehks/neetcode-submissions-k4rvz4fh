605a76ae23d66a6696b0a36b68dddf8f1f63769e
Hein Kyaw Soe <142381377+henryleehks@users.noreply.github.com>
2026-04-07 22:44:00 +0800

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+        if s_dict == t_dict:
+            return True
+        return False
